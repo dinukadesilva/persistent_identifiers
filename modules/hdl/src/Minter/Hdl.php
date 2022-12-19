6f600ca4bce4b7cba8111e118197696a4d9e215a
Dinuka De Silva <l.dinukadesilva@gmail.com>
2022-12-19 07:50:46 -0500

    Handle methods

--- modules/hdl/src/Minter/Hdl.php
+++ modules/hdl/src/Minter/Hdl.php
@@ -3,6 +3,7 @@
 namespace Drupal\hdl\Minter;
 
 use Drupal\persistent_identifiers\MinterInterface;
+use function mysql_xdevapi\getSession;
 
 /**
  * A Handle class.
@@ -53,46 +54,105 @@ class Hdl implements MinterInterface
   public function mint($entity, $extra = NULL)
   {
     $config = \Drupal::config('hdl.settings');
-    $handle_prefix = $config->get('hdl_prefix');
-
-    if ($extra && array_key_exists('hdl_qualifier', $extra)) {
-      $handle_type_qualifier = $extra['hdl_qualifier'];
-    } else {
-      $handle_type_qualifier = $config->get('hdl_qualifier');
-    }
-    $handle = $handle_prefix . '/';// . $handle_type_qualifier . '.' . $entity->id();
-    $host = \Drupal::request()->getSchemeAndHttpHost();
-    $url = "http://example.com"; //$host . $entity->toUrl()->toString();
+    // $url = "http://example.com"; //$host . $entity->toUrl()->toString();
     $admin_handle = $config->get('hdl_admin_handle');
-    $handle_admin_index = $config->get('hdl_admin_index');
     $endpoint_url = $config->get('hdl_handle_api_endpoint');
-    $permissions = $config->get('hdl_handle_permissions');
-    $password = $config->get('hdl_handle_basic_auth_password');
-    $private_key_pem = $password; // $config->get('hdl_handle_private_key');
     $handle_json = [
       [
         'index' => 1,
         'type' => "URL",
         'data' => [
           'format' => "string",
-          'value' => $url,
+          'value' => $extra["url"] ?? "",
         ],
-      ],
-      [
-        'index' => 100,
-        'type' => 'HS_ADMIN',
-        'data' => [
-          'format' => 'admin',
-          'value' => [
-            'handle' => $admin_handle,
-            'index' => $handle_admin_index,
-            'permissions' => $permissions,
-          ],
-        ],
-      ],
+      ]
     ];
 
     \Drupal::logger('persistent identifiers')->debug("##### Dinuka signature start");
+
+    $sessionId = $this->getSessionId();
+    $response3 = \Drupal::httpClient()->PUT($endpoint_url . "/api/handles/" . $admin_handle . "/?overwrite=false&mintNewSuffix=true", [
+      'headers' => [
+        'Authorization' => 'Handle sessionId="' . $sessionId . '"',
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+      ],
+      'json' => $handle_json
+    ]);
+    $response3_body_array = json_decode($response3->getBody()->getContents(), TRUE);
+
+    return $response3_body_array["handle"];
+  }
+
+  public function save($handle, $extra = NULL)
+  {
+    $config = \Drupal::config('hdl.settings');
+    $admin_handle = $config->get('hdl_admin_handle');
+    $endpoint_url = $config->get('hdl_handle_api_endpoint');
+    $handle_json = [
+      [
+        'index' => 1,
+        'type' => "URL",
+        'data' => [
+          'format' => "string",
+          'value' => $extra["url"] ?? "",
+        ],
+      ]
+    ];
+
+    \Drupal::logger('persistent identifiers')->debug("##### Dinuka signature start");
+
+    $sessionId = $this->getSessionId();
+    $response3 = \Drupal::httpClient()->PUT($endpoint_url . "/api/handles/" . $admin_handle . "/?overwrite=false&mintNewSuffix=true", [
+      'headers' => [
+        'Authorization' => 'Handle sessionId="' . $sessionId . '"',
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+      ],
+      'json' => $handle_json
+    ]);
+    $response3_body_array = json_decode($response3->getBody()->getContents(), TRUE);
+
+    return $response3_body_array["handle"];
+  }
+
+  public function fetch($handle)
+  {
+    $config = \Drupal::config('hdl.settings');
+    $endpoint_url = $config->get('hdl_handle_api_endpoint');
+    $sessionId = $this->getSessionId();
+    $response3 = \Drupal::httpClient()->GET($endpoint_url . "/api/handles/" . $handle, [
+      'headers' => [
+        'Authorization' => 'Handle sessionId="' . $sessionId . '"',
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+      ]
+    ]);
+    $response3_body_array = json_decode($response3->getBody()->getContents(), TRUE);
+
+
+    \Drupal::logger('persistent identifiers')->debug("##### handle Fetch " . $handle . " : " . json_encode($response3_body_array));
+
+    if (isset($response3_body_array["values"])) {
+      foreach ($response3_body_array["values"] as $value) {
+        if ($value["type"] == "URL") {
+          return ["url" => $value["data"]["value"]];
+        }
+      }
+    }
+
+    return ["url" => NULL];
+  }
+
+  public function getSessionId()
+  {
+    $config = \Drupal::config('hdl.settings');
+    $endpoint_url = $config->get('hdl_handle_api_endpoint');
+    $admin_handle = $config->get('hdl_admin_handle');
+    $handle_admin_index = $config->get('hdl_admin_index');
+    $password = $config->get('hdl_handle_basic_auth_password');
+    $private_key_pem = $password; // $config->get('hdl_handle_private_key');
+
     $cnonce = $this->generateRandomString(16);
 
     $response1 = \Drupal::httpClient()->post($endpoint_url . "/api/sessions", [
@@ -120,17 +180,7 @@ class Hdl implements MinterInterface
     ]);
     $response2_body_array = json_decode($response2->getBody()->getContents(), TRUE);
 
-    $response3 = \Drupal::httpClient()->PUT($endpoint_url . "/api/handles/" . $admin_handle . "/?overwrite=false&mintNewSuffix=true", [
-      'headers' => [
-        'Authorization' => 'Handle sessionId="' . $sessionId . '"',
-        'Content-Type' => 'application/json',
-        'Accept' => 'application/json'
-      ],
-      'json' => $handle_json
-    ]);
-    $response3_body_array = json_decode($response3->getBody()->getContents(), TRUE);
-
-    return $response3_body_array["handle"];
+    return $sessionId;
   }
 
   public function generateRandomString($length = 10)
